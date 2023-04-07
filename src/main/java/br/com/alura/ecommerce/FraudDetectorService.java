d056b096859eba0f1fd0e8f59afff8e66f4e3bde
Lucas Oliveira <lcsoliveira@ibm.com>
2023-04-07 00:25:55 -0300

    Refatorando o código

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,6 +2,7 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -14,48 +15,24 @@ import java.util.UUID;
 public class FraudDetectorService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        /*
-            we could pass more than one topic to consumer.subscribe(),
-            but it is rarely used in most cases each service do only one thing
-        */
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("Founded " + records.count() + " records");
-                for (var record : records) {
-                    System.out.println("---------------------------------------------");
-                    System.out.println("Processing new Order, checking for fraud");
-                    System.out.println("KEY: " + record.key());
-                    System.out.println("VALUE: " + record.value());
-                    System.out.println("PARTITION: " + record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order Processed!!!");
-                }
-            }
-        }
+        var fraudService = new FraudDetectorService();
+        var service = new KafkaService(FraudDetectorService.class.getSimpleName(),"ECOMMERCE_NEW_ORDER", fraudService::parse);
+        service.run();
     }
 
-    private static Properties properties() {
-        /*
-            If two or more services has the same GROUP name
-            then the messages will be distributed between them, and you don't know each one will receive the message
-        */
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
-        // Maximo de records que posso consumir por ver
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-        return properties;
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("---------------------------------------------");
+        System.out.println("Processing new Order, checking for fraud");
+        System.out.println("KEY: " + record.key());
+        System.out.println("VALUE: " + record.value());
+        System.out.println("PARTITION: " + record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order Processed!!!");
     }
 }
--- src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -1,4 +1,50 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
 public class KafkaService {
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
+
+    public KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this.consumer = new KafkaConsumer<>(properties(groupId));
+        this.parse = parse;
+
+        //we could pass more than one topic to consumer.subscribe(),but it is rarely used in most cases each service do only one thing
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+     void run () {
+        while(true) {
+            // Esta prop records é do ConsumerRecords
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("Founded " + records.count() + " records");
+                for (var record : records) {
+                    parse.consume(record);
+                }
+            }
+        }
+    }
+
+    private static Properties properties(String groupId) {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        // Maximo de records que posso consumir por vez
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        // If two or more services has the same GROUP name
+        // then the messages will be distributed between them, and you don't know each one will receive the message
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        return properties;
+    }
 }
